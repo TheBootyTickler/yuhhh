6fb5acdeb101a225b754ed275319d6fb8442ac1f
TheBootyTickler <102940343+TheBootyTickler@users.noreply.github.com>
2022-06-14 07:21:36 -0500

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,53 @@
+e u s u 0 r o r
+e u s u 0 r o r
+e u s u 0 r o r
+e u s u 0 r o r
+e u [xs] u [z0] [rm] [on] [rn]
+e [um] [sm] u [z0] [rm] [zo] [zr]
+e [un] [sn] u [b0] [vr] [ob] [rb]
+e [zu] [sn] u 0 r o r
+e u [xsm] u [zn0] [rmb] [von] [vrn]
+e [umb] [smb] u [zn0] [rmb] [zon] [zrn]
+e [xum] [xsm] u [zn0] [rmb] [zon] [zrn]
+e [xvu] [xsm] u 0 r o r
+y p [gc] [pc] [tc] [pc] [xf] [pc]
+[yx] [zp] g p y o d o
+t o [xf] [xo] [xr] [xo] [zf] [xo]
+[ze] [pl] s [us] [rd]| [tf]|
+y p [dcb] [pcb]
+[tcb] [pcb] [xvg] [pcb]
+[rcb] [zpc] d p r p d p
+t p [xvf] [xvp]
+[xvr] [xvp] [zfc] [xpm]
+[zec] [xvp] f p 0 u o u [yspi]|||
+[spif] [uoda] [yspi] [ypia] [yspi]|||
+[yspi] [uoda] [yspi] [uoda] [spif]|||
+[spif] [uoda] [yspi] [utoa] [yrpi]|||
+eryip|
+e u s u 0 r o r
+e u s u 0 r o r
+e u s u 0 r o r
+e u s u 0 r o r
+e u [xs] u [z0] [rm] [on] [rn]
+e [um] [sm] u [z0] [rm] [zo] [zr]
+e [xu] [xs] u [z0] [rm] [zo] [zr]
+e [vu] [xs] u 0 r o r e u [xsm] u
+[zn0] [rmb] [von] [vrn] e [umb] [smb] u
+[zn0] [rmb] [zon] [zrn] e [xum] [xsm] u
+[zn0] [rmb] [zon] [zrn] e [xvu] [xsm] u
+0 r o r y p [gc] [pc]
+[tc] [pc] [xf] [pc]
+[yx] [zp] g p
+y o d o t o [xf] [xo]
+[xr] [xo] [zf] [xo]
+[ze] [pl] s [us] [rd]| [tf]|
+y p [dcb] [pcb]
+[tcb] [pcb] [xvg] [pcb]
+[rcb] [zpc] d p
+r p d p t p [xvf] [xvp]
+[xvr] [xvp] [zfc] [xpm]
+[zec] [xvp] f p 0 u o u [yspi]|||
+[spif] [uoda] [yspi] [ypia] [yspi]|||
+[yspi] [uoda] [yspi] [uoda] [spif]|||
+[spif] [uoda] [yspi] [utoa] [yrpi]|||
+eryip
